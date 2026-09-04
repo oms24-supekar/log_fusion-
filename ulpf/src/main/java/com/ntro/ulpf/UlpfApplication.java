e33989f567668ab356bb646fd03ca8306bc18a7f
oms24-supekar <omsupekar24@gmail.com>
2026-09-04 11:11:23 +0530

    Initialize Spring Boot backend

---
+++ ulpf/src/main/java/com/ntro/ulpf/UlpfApplication.java
@@ -0,0 +1,13 @@
+package com.ntro.ulpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlpfApplication.class, args);
+	}
+
+}
---
+++ ulpf/src/test/java/com/ntro/ulpf/UlpfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntro.ulpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
