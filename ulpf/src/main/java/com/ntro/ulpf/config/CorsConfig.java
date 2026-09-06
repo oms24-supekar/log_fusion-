5cdc28a3be9739c6acb998c42fe71e0e76e844ec
oms24-supekar <omsupekar24@gmail.com>
2026-09-06 22:40:51 +0530

    changes

--- ulpf/src/main/java/com/ntro/ulpf/config/CorsConfig.java
+++ ulpf/src/main/java/com/ntro/ulpf/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ntro.ulpf.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,20 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
-
             @Override
-            public void addCorsMappings(
-                    CorsRegistry registry
-            ) {
-
+            public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/api/**")
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173",
+                                frontendUrl
                         )
                         .allowedMethods(
                                 "GET",
